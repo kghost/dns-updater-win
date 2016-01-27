67e94ff312eae2ce59b4f94c84ffb97e45be22d5
Zang MingJie <zealot0630@gmail.com>
2016-01-27 16:26:48 +0800

    init

---
+++ Updater/Updater.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Updater
+{
+    partial class updater
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateTimer = new System.Timers.Timer();
+            this.logger = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.updateTimer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logger)).BeginInit();
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Enabled = true;
+            this.updateTimer.Interval = 120000D;
+            this.updateTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.updateTimer_Elapsed);
+            // 
+            // logger
+            // 
+            this.logger.Log = "Application";
+            this.logger.Source = "DNS Updater";
+            // 
+            // updater
+            // 
+            this.ServiceName = "localDnsUpdater";
+            ((System.ComponentModel.ISupportInitialize)(this.updateTimer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logger)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Timers.Timer updateTimer;
+        private System.Diagnostics.EventLog logger;
+    }
+}
---
+++ Updater/Updater.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater
+{
+    public partial class updater : ServiceBase
+    {
+        public updater()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.updateTimer.Start();
+            System.Net.NetworkInformation.NetworkChange.NetworkAddressChanged += addressChangedCallback;
+        }
+
+        protected override void OnStop()
+        {
+            System.Net.NetworkInformation.NetworkChange.NetworkAddressChanged -= addressChangedCallback;
+            this.updateTimer.Stop();
+        }
+
+        private void addressChangedCallback(Object sender, EventArgs e)
+        {
+            checkUpdate();
+        }
+
+        private void updateTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            checkUpdate();
+        }
+
+        private void checkUpdate()
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+
+                System.Net.NetworkInformation.NetworkInterface nic = null;
+                if (settings["network"] != null)
+                {
+                    var name = settings["network"].Value;
+                    foreach (var n in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+                    {
+                        if (n.Name == name)
+                        {
+                            nic = n;
+                            break;
+                        }
+                    }
+                }
+
+                if (nic == null)
+                {
+                    foreach (var n in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+                    {
+                        var properties = n.GetIPProperties();
+                        if (properties == null) continue;
+                        var ipv4 = properties.GetIPv4Properties();
+                        if (ipv4 == null) continue;
+                        if (ipv4.IsDhcpEnabled)
+                        {
+                            var hasip = false;
+                            foreach (var address in nic.GetIPProperties().UnicastAddresses)
+                            {
+                                if (address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                                {
+                                    hasip = true;
+                                    break;
+                                }
+                            }
+
+                            if (hasip)
+                            {
+                                nic = n;
+                                settings["network"].Value = n.Name;
+                                configFile.Save(ConfigurationSaveMode.Modified);
+                                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (nic == null) return;
+
+                var host = settings["host"].Value;
+                var domain = settings["domain"].Value;
+                System.Net.NetworkInformation.IPAddressInformation ip = null;
+                foreach (var address in nic.GetIPProperties().UnicastAddresses)
+                {
+                    if (address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        ip = address;
+                    }
+                }
+
+                var skip = false;
+                try
+                {
+                    foreach (var current in Dns.GetHostEntry(host + "." + domain).AddressList)
+                    {
+                        if (current.Equals(ip.Address))
+                        {
+                            skip = true;
+                            break;
+                        }
+                    }
+                }
+                catch (System.Net.Sockets.SocketException)
+                {
+                }
+
+                if (!skip)
+                {
+                    var address = ip.Address.ToString();
+                    var request = WebRequest.Create(string.Format("http://kghost.info/update?host={0}&ip={1}", host, address));
+                    var response = request.GetResponse();
+                    var reader = new StreamReader(response.GetResponseStream());
+                    this.logger.WriteEntry(string.Format("Update {0} to {1}: {2}", host, address, reader.ReadToEnd()), EventLogEntryType.Information);
+                    reader.Close();
+                    response.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                this.logger.WriteEntry(e.ToString(), EventLogEntryType.Error);
+            }
+        }
+    }
+}
